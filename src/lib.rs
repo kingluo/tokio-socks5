61e079b3a7d6c89196b0c26c05b990db39622fc4
Jinhua Luo <home_king@163.com>
2019-10-12 19:50:17 +0800

    remove blocking dns resolving
    Because tokio `TcpStream::connect` already support host name.

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,11 @@ use byteorder::{BigEndian, ByteOrder};
 
 use std::io;
 use std::io::Result;
+use std::net::SocketAddr;
 use std::net::{IpAddr, Ipv4Addr};
-use std::net::{SocketAddr, ToSocketAddrs};
-use std::pin::Pin;
-use std::task::{Context, Poll};
 
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
-use tokio_executor::threadpool::blocking;
 
 pub trait Encoder: Send + Sync {
     fn encode(&mut self, buf: &[u8]) -> &[u8];
@@ -70,29 +67,11 @@ async fn copy<'a, T: AsyncRead + Unpin, U: AsyncWrite + Unpin>(
     loop {
         let len = read(stream1, encoder1, &mut buf).await?;
         if len == 0 {
-            println!("socket broken");
             break;
         }
         write_all(stream2, encoder2, &buf[..len]).await?
     }
     Ok(())
-}
-
-struct ToSockAddrsFuture {
-    addr: String,
-}
-
-impl Future for ToSockAddrsFuture {
-    type Output = io::Result<SocketAddr>;
-
-    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
-        blocking(|| {
-            println!("resolving {}", self.addr);
-            let mut addrs_iter = self.addr.to_socket_addrs().unwrap();
-            addrs_iter.next().unwrap()
-        })
-        .map_err(|err| panic!("Error in blocing block: {:?}", err))
-    }
 }
 
 async fn handle(mut stream: TcpStream, mut encoder: Option<Box<dyn Encoder>>) -> Result<()> {
@@ -132,7 +111,7 @@ async fn handle(mut stream: TcpStream, mut encoder: Option<Box<dyn Encoder>>) ->
         return Ok(());
     }
 
-    let addr: SocketAddr;
+    let addr;
     match atyp {
         1 => {
             if len != 10 {
@@ -141,7 +120,7 @@ async fn handle(mut stream: TcpStream, mut encoder: Option<Box<dyn Encoder>>) ->
             }
             let dst_addr = IpAddr::V4(Ipv4Addr::new(buf[4], buf[5], buf[6], buf[7]));
             let dst_port = BigEndian::read_u16(&buf[8..]);
-            addr = SocketAddr::new(dst_addr, dst_port);
+            addr = SocketAddr::new(dst_addr, dst_port).to_string();
         }
         3 => {
             let offset = 4 + 1 + (buf[4] as usize);
@@ -153,7 +132,7 @@ async fn handle(mut stream: TcpStream, mut encoder: Option<Box<dyn Encoder>>) ->
             let mut dst_addr = std::str::from_utf8(&buf[5..offset]).unwrap().to_string();
             dst_addr.push_str(":");
             dst_addr.push_str(&dst_port.to_string());
-            addr = ToSockAddrsFuture { addr: dst_addr }.await.unwrap();
+            addr = dst_addr;
         }
         _ => {
             println!("Address type not supported, type={}", atyp);
@@ -168,7 +147,7 @@ async fn handle(mut stream: TcpStream, mut encoder: Option<Box<dyn Encoder>>) ->
     }
 
     println!("incoming socket, request upstream: {:?}", addr);
-    let up_stream = match TcpStream::connect(&addr).await {
+    let up_stream = match TcpStream::connect(addr).await {
         Ok(s) => s,
         Err(e) => {
             println!("Upstream connect failed, {}", e);
@@ -192,20 +171,19 @@ async fn handle(mut stream: TcpStream, mut encoder: Option<Box<dyn Encoder>>) ->
     let (mut ri, mut wi) = stream.split();
     let (mut ro, mut wo) = up_stream.split();
 
-    let mut encoder1 = encoder.as_ref().map(|e| e.clone());
-    let mut encoder3 = None;
+    let mut encoder2 = None;
+    let mut encoder4 = encoder.as_ref().map(|e| e.clone());
     tokio::spawn(async move {
-        copy(&mut ri, &mut wo, &mut encoder1, &mut encoder3)
+        copy(&mut ro, &mut wi, &mut encoder2, &mut encoder4)
             .await
             .unwrap();
     });
 
-    let mut encoder2 = None;
-    let mut encoder4 = encoder.as_ref().map(|e| e.clone());
-    copy(&mut ro, &mut wi, &mut encoder2, &mut encoder4)
+    let mut encoder1 = encoder.as_ref().map(|e| e.clone());
+    let mut encoder3 = None;
+    copy(&mut ri, &mut wo, &mut encoder1, &mut encoder3)
         .await
         .unwrap();
-
     return Ok(());
 }
 
